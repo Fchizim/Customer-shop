6b93188b032abeabd79a0c6b25f827f5f928cdf4
Fred <104666842+Fchizim@users.noreply.github.com>
2023-10-24 07:16:26 +0100

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,4 @@
+const customerName = prompt('What is your name?');
+const customerOrder = prompt('What would you like to order, we have (Rice, Bean and yam)?');
+const amountOrder = prompt('How many plates?');
+alert(customerName + ' wants ' + amountOrder +' plates of ' + customerOrder);
